f81b97b5cde867ccbd1e7f274925061fa41b74f0
KMDMNAK <kmdmuneaki@gmail.com>
2019-12-10 01:39:27 +0900

    add material ui

--- src/components/toppage/Navigation.tsx
+++ src/components/toppage/Navigation.tsx
@@ -1,31 +1,47 @@
-import React from 'react';
-import styles from './style.css';
-import ids from './ids.json';
-
+import React from "react";
+import styles from "./style.css";
+import ids from "./ids.json";
+import Button from "@material-ui/core/Button";
 const Navigation = () => {
-    return (
-        <div id="navigation">
-            <span className={styles.navigation_button}>
-                <a href={"#" + ids.column_about}>
-                    ABOUT
-                </a>
-            </span>
-            <span className={styles.navigation_button}>
-                <a href={"#" + ids.column_blog}>
-                    BLOG
-                </a>
-            </span>
-            <span className={styles.navigation_button}>
-                <a href={"#" + ids.column_projects}>
-                    Projects
-                </a>
-            </span>
-            <span className={styles.navigation_button}>
-                <a href={"#" + ids.column_notes}>
-                    Notes
-                </a>
-            </span>
-        </div>
-    )
-}
+  return (
+    <div id={styles.navigation}>
+      <span className={styles.navigation_button}>
+        <Button
+          variant="contained"
+          color="secondary"
+          href={"#" + ids.column_about}
+        >
+          ABOUT
+        </Button>
+      </span>
+      <span className={styles.navigation_button}>
+        <Button
+          variant="contained"
+          color="secondary"
+          href={"#" + ids.column_blog}
+        >
+          BLOG
+        </Button>
+      </span>
+      <span className={styles.navigation_button}>
+        <Button
+          variant="contained"
+          color="secondary"
+          href={"#" + ids.column_projects}
+        >
+          Projects
+        </Button>
+      </span>
+      <span className={styles.navigation_button}>
+        <Button
+          variant="contained"
+          color="secondary"
+          href={"#" + ids.column_notes}
+        >
+          Notes
+        </Button>
+      </span>
+    </div>
+  );
+};
 export default Navigation;
--- src/components/toppage/style.css.d.ts
+++ src/components/toppage/style.css.d.ts
@@ -2,6 +2,7 @@ declare const styles: {
   readonly "column": string;
   readonly "navigation_button": string;
   readonly "column_content": string;
+  readonly "navigation": string;
 };
 export = styles;
 
