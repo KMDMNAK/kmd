889632ab6e91f8eb2d9803d9b4bde06459216887
KMDMNAK <kmdmuneaki@gmail.com>
2020-05-20 19:59:15 +0900

    Use environmental variable for apollo server

--- client/src/utils/ApolloClient.ts
+++ client/src/utils/ApolloClient.ts
@@ -3,8 +3,10 @@ import fetch from 'isomorphic-unfetch'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import ApolloClient from 'apollo-client';
+const APOLLO_SERVER = process.env.APOLLO_SERVER
+console.debug({ APOLLO_SERVER })
 export const createApolloClient = () => {
-    const endpoint = "https://kmdserver.kmdmnak.now.sh/graphql"
+    const endpoint = APOLLO_SERVER || "https://kmdserver.kmdmnak.now.sh/graphql"
     const cache = new InMemoryCache();
     const link = new HttpLink({
         uri: endpoint,
