a394ee68cefb1fda5f69ff407ea2f071c975fbaa
KMDMNAK <kmdmuneaki@gmail.com>
2020-05-20 20:00:42 +0900

    Add Support for logging

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { RequestHandler, ParamsDictionary } from 'express-serve-static-core'
 import Apollo, { ApolloServer, gql } from 'apollo-server-express';
 import Resolvers from './api/resolvers'
 import typeDefs from './api/scheme.gql'
@@ -7,8 +8,15 @@ const app = express();
 const PORT_NUMBER = 4000;
 
 const resolvers: Apollo.IResolvers<any, any> = Resolvers as Apollo.IResolvers<any, any>;
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers, debug: true })
 
+const logMiddleware: RequestHandler<ParamsDictionary, any, any> = (req, res, next) => {
+    console.log({
+        route: req.url
+    })
+    next()
+}
+app.use(logMiddleware)
 server.applyMiddleware({ app })
 
 app.listen({ port: PORT_NUMBER }, () => {
