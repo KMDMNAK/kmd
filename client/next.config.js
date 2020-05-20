bad7dd833c020dcf4c44b0b6810d1cea0d4b2c05
KMDMNAK <kmdmuneaki@gmail.com>
2020-05-20 22:51:47 +0900

    In case

--- client/next.config.js
+++ client/next.config.js
@@ -1,71 +1,53 @@
-
 const remarkMath = require('remark-math')
 const rehypeKatex = require('rehype-katex')
 
 const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex]
-  }
-})
-// https://github.com/mdx-js/mdx/issues/465
+        extension: /\.mdx?$/,
+        options: {
+            remarkPlugins: [remarkMath],
+            rehypePlugins: [rehypeKatex]
+        }
+    })
+    // https://github.com/mdx-js/mdx/issues/465
 const withCSS = require('@zeit/next-css')
 const withSourceMaps = require('@zeit/next-source-maps')
-//const withPlugins = require('next-compose-plugins');
-/*
-module.exports = withPlugins([
-  [withSourceMaps],
-  [withCSS],
-  [withMDX]
-], {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
-  ,
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-  },
-  webpack: (config, { dev }) => {
-    config.module.rules.push({
-      test: /\.(graphql|gql)$/,
-      exclude: /node_modules/,
-      loader: 'graphql-tag/loader'
-    });
-    if (dev) {
-      config.watchOptions = {
-        poll: true
-      }
-    }
-    config.resolve.extensions = [".js", ".jsx", ".ts", ".tsx", ".json", ".css", ".md", ".mdx"];
-    return config
-  }
-});
-*/
+
+const internalNodeModulesRegExp = /src(?!\/(?!.*js))/
+const externalNodeModulesRegExp = /node_modules(?!\/@zeit(?!.*node_modules))/
 
 module.exports =
-  withSourceMaps(
-    withCSS(
-      withMDX({
-        pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-        cssModules: true,
-        cssLoaderOptions: {
-          importLoaders: 1,
-        },
-        webpack: (config, { dev }) => {
-          config.module.rules.push({
-            test: /\.(graphql|gql)$/,
-            exclude: /node_modules/,
-            loader: 'graphql-tag/loader'
-          });
-          if (dev) {
-            config.watchOptions = {
-              poll: true
-            }
-          }
-          config.resolve.extensions = [".js", ".jsx", ".ts", ".tsx", ".json", ".css", ".md", ".mdx"];
-          return config
-        }
-      })
-    )
-  )
 
+    withCSS(
+        withMDX({
+            pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+            cssModules: true,
+            cssLoaderOptions: {
+                importLoaders: 1,
+            },
+            webpack: (config, { dev, webpack, defaultLoaders }) => {
+                config.module.rules.push({
+                    test: /\.(graphql|gql)$/,
+                    exclude: /node_modules/,
+                    loader: 'graphql-tag/loader'
+                });
+                if (dev) {
+                    config.watchOptions = {
+                        poll: true
+                    }
+                }
+                config.resolve.extensions = [".js", ".jsx", ".ts", ".tsx", ".json", ".css", ".md", ".mdx"];
+                config.externals = config.externals.map(external => {
+                    if (typeof external !== "function") return external
+                    return (ctx, req, cb) => (internalNodeModulesRegExp.test(req) ? cb() : external(ctx, req, cb))
+                })
+
+                // defaultLoaders.babel.options.plugins.push(["transform-define"])
+                config.module.rules.push({
+                    test: /\.+(js|jsx)$/,
+                    use: defaultLoaders.babel,
+                    include: [internalNodeModulesRegExp],
+                })
+                return config
+            }
+        })
+    )
