7858d1226a35ae8a93ee01f0960cc69e3ce42139
KMDMNAK <kmdmuneaki@gmail.com>
2020-05-20 20:00:18 +0900

    Add support for logging.

--- server/src/api/query.ts
+++ server/src/api/query.ts
@@ -2,14 +2,18 @@
 import { QueryResolvers, ArticleInfo, QueryGetBlogArticlesArgs } from './resolverType'
 import getBlogArticles from '../utils/queryBlog'
 import getNotesArticles from '../utils/queryNotes'
-
+const beforeMethod = (methodName: string) => {
+    console.log({ methodName })
+}
 export const Query: QueryResolvers = {
     getCurrentArticleList: async (obj: any, args: any, context: any, info: any) => {
+        beforeMethod("getCurrentArticleList")
         const limit = 3;
         const offset = 0;
         return await getBlogArticles(limit, offset)
     },
     getBlogArticles: async (obj: any, args: QueryGetBlogArticlesArgs, context: any, info: any) => {
+        beforeMethod("getBlogArticles")
         const offset = args.list_amount * args.page;
         const limit = args.list_amount;
         return await getBlogArticles(limit, offset)
