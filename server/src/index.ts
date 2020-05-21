ae2931eaac69fe4b724db7391defb26e93434a0c
KMDMNAK <kmdmuneaki@gmail.com>
2020-05-21 20:11:20 +0900

    Divide module and add debug.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,7 @@
-import express, { Router } from 'express';
-import { RequestHandler, ParamsDictionary } from 'express-serve-static-core'
-import Apollo, { ApolloServer, gql } from 'apollo-server-express';
-import Resolvers from './api/resolvers'
-import typeDefs from './api/scheme.gql'
+import app from './server'
 import { PORT } from './environment'
 
-const app = express();
-
-const resolvers: Apollo.IResolvers<any, any> = Resolvers as Apollo.IResolvers<any, any>;
-const server = new ApolloServer({ typeDefs, resolvers, debug: true })
-const logMiddleware: RequestHandler<ParamsDictionary, any, any> = (req, res, next) => {
-    console.log({
-        route: req.url
-    })
-    next()
-
-}
-const api = Router()
-api.use(logMiddleware)
-server.applyMiddleware({ app, path: '/api/graphql' })
-
-app.use('/api', api)
-
 app.listen({ port: PORT }, () => {
-    console.log(`server's graphql path is  ${server.graphqlPath}`);
+    // console.log(`server's graphql path is  ${server.graphqlPath}`);
     console.log(`server starts at port number:${PORT}`);
 })
