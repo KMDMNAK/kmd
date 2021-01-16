56f0b1549d737bfcb7c3a0bc392edce9875b5d55
KMDMNAK <kmdmuneaki@gmail.com>
2021-01-16 21:30:55 +0900

    update

--- client/src/pages/about/Chart.tsx
+++ client/src/pages/about/Chart.tsx
@@ -1,23 +1,35 @@
 // import Charts from "react-frappe-charts"
-const frappe = require('frappe-charts')
+// const frappe = require('frappe-charts')
+import dynamic from 'next/dynamic'
+const Charts = dynamic(import('react-frappe-charts'), { ssr: false })
 
 const mockEndDate = new Date()
 const mockStartDate = new Date()
 mockStartDate.setDate(mockStartDate.getDate() - 100);
 
+const mockMidDate = new Date()
+mockMidDate.setDate(mockStartDate.getDate() - 50);
 
-// const Component = () => {
-//     return (<>
-//         <Charts
-//             type="heatmap"
-//             data={{
-//                 start: mockStartDate,
-//                 end: mockEndDate
-//             }}
-//             colors={['#ebedf0', '#c0ddf9', '#73b3f3', '#3886e1', '#17459e']}
-//         />
-//     </>)
-// }
+
+const Component = () => {
+	console.debug({ mockStartDate })
+	console.debug({ mockEndDate })
+	console.debug({ mockMidDate })
+    return (<>
+        <Charts
+            type="heatmap"
+            data={{
+                dataPoints: {
+                    [String(`${mockStartDate.getTime()}`).slice(0,10)]: 40,
+                    [String(`${mockMidDate.getTime()}`).slice(0,10)]: 40
+                },
+                start: mockStartDate,
+                end: mockEndDate,
+            }}
+            colors={['#ebedf0', '#c0ddf9', '#73b3f3', '#3886e1', '#17459e']}
+        />
+    </>)
+}
 
 
 const data = {
@@ -36,18 +48,18 @@ const data = {
     ]
 }
 
-const chart = new frappe.Chart("#chart", {  // or a DOM element,
-    // new Chart() in case of ES6 module with above usage
-    title: "My Awesome Chart",
-    data: data,
-    type: 'axis-mixed', // or 'bar', 'line', 'scatter', 'pie', 'percentage'
-    height: 250,
-    colors: ['#7cd6fd', '#743ee2']
-})
+// const chart = new frappe.Chart("#chart", {  // or a DOM element,
+//     // new Chart() in case of ES6 module with above usage
+//     title: "My Awesome Chart",
+//     data: data,
+//     type: 'axis-mixed', // or 'bar', 'line', 'scatter', 'pie', 'percentage'
+//     height: 250,
+//     colors: ['#7cd6fd', '#743ee2']
+// })
 
 
-const Component = () => {
-    return (<div id="chart"></div>)
-}
+// const Component = () => {
+//     return (<div id="chart"></div>)
+// }
 
 export default Component
--- client/src/pages/about/index.tsx
+++ client/src/pages/about/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { withChakra } from '../../utils/Chakra'
+import { Text } from '@chakra-ui/react'
 
 import Template from '../../components/PageTemplate'
 
@@ -9,19 +10,25 @@ import Chart from './Chart'
 const Content = () => {
     return (
         <>
-            <div style={{textAlign:'center'}}>
-                This is About
-            </div>
-            <div style={{textAlign:'center'}}>
+            <Text fontFamily="ＭＳ Ｐ明朝">
+                <p>大学では数理情報科学を学び、2020年3月に大学を卒業しました。</p>
+                <p>機械学習プラットフォームの提供を行う会社に新卒で入社し、クライアント(Vuejs)、サーバー(Nodejs,Koa)、クラウド(firebase)、機械学習の実行用コンテナ(Python)等の作成運用を行っていました。</p>
+                <p>当ページはNextjsで作成で作成しています。</p>
+            </Text>
+            <div style={{ display: "flex", justifyContent: 'center', alignItems: 'center' }}>
                 <Chart />
             </div>
         </>
     )
 }
-const Page = () => {
+const Page: React.FC<{}> = props => {
+    useEffect(() => {
+        console.debug('This is useEffect in about')
+    })
     return (<>
         <Template title="About" Content={Content} />
     </>)
 }
+
 
 export default withChakra(Page)
