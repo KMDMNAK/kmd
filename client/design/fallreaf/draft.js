8de497837a091023803bb98f938d01755ba4f654
KMDMNAK <kmdmuneaki@gmail.com>
2021-05-29 18:37:00 +0900

    add fall read effect

---
+++ client/design/fallreaf/draft.js
@@ -0,0 +1,9 @@
+const hamburger1s = document.getElementsByClassName('hamburger1')
+/**
+ * @type {HTMLElement}
+ */
+const hamburger1 = Array.from(hamburger1s)[0]
+const sidebar = document.getElementById('sidebar')
+hamburger1.addEventListener('mousedown', () => {
+
+})
